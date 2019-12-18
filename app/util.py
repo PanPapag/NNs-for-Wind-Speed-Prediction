ffdfdae072ca63c9988b52de2afa52197125944f
Papageorgiou <pantelis.papageorgiou@nokia.com>
2019-12-18 15:47:41 +0200

    model completed

--- app/util.py
+++ app/util.py
@@ -1,7 +1,12 @@
 from keras.models import Sequential
 from keras.layers import Dense
 
+# Define Neural Network model
 def get_model():
+    # create model
     model = Sequential()
-
+    model.add(Dense(64, input_dim=128, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    # compile model
+    model.compile(loss='mean_absolute_error', optimizer='adam')
     return model
